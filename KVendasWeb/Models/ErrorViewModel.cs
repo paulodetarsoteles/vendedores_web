eff272c4b083e1a0d423edfb961b6db05c0963a7
paulodetarsoteles <potassio@hotmail.com>
2022-03-11 22:26:59 -0300

    Criacao do projeto

---
+++ KVendasWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KVendasWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
