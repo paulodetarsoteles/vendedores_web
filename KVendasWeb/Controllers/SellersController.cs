e81f3cf96f8c0869329446cf9132eee6e064eec2
paulodetarsoteles <potassio@hotmail.com>
2022-03-20 13:03:07 -0300

    Delete Vendedores

--- KVendasWeb/Controllers/SellersController.cs
+++ KVendasWeb/Controllers/SellersController.cs
@@ -35,5 +35,28 @@ namespace KVendasWeb.Controllers
             _sellerServices.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerServices.FindById(id.Value); 
+            if (obj == null)
+            {
+                return NotFound(); 
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerServices.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
