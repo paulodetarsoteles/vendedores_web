276822dc1c80c9a2ed949be5a59186949cfcef27
paulodetarsoteles <potassio@hotmail.com>
2022-03-20 17:37:51 -0300

    Criacao do Edit

--- KVendasWeb/Controllers/SellersController.cs
+++ KVendasWeb/Controllers/SellersController.cs
@@ -1,20 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using KVendasWeb.Services;
 using KVendasWeb.Models;
-using KVendasWeb.Models.ViewModels; 
+using KVendasWeb.Models.ViewModels;
+using System.Collections.Generic;
+using KVendasWeb.Services.Exceptions;
 
 namespace KVendasWeb.Controllers
 {
     public class SellersController : Controller
     {
-        private readonly SellerService _sellerServices; 
+        private readonly SellerService _sellerServices;
         private readonly DepartamentService _departamentService;
 
-        public SellersController (SellerService sellerService, DepartamentService departamentService)
+        public SellersController(SellerService sellerService, DepartamentService departamentService)
         {
             _sellerServices = sellerService;
             _departamentService = departamentService;
         }
+
         public IActionResult Index()
         {
             var list = _sellerServices.FindAll();
@@ -35,29 +38,6 @@ namespace KVendasWeb.Controllers
             _sellerServices.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
-        public IActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var obj = _sellerServices.FindById(id.Value); 
-            if (obj == null)
-            {
-                return NotFound(); 
-            }
-            else
-            {
-                return View(obj);
-            }
-        }
-        [HttpPost]
-        [AutoValidateAntiforgeryToken]
-        public IActionResult Delete(int id)
-        {
-            _sellerServices.Remove(id);
-            return RedirectToAction(nameof(Index));
-        }
 
         public IActionResult Details(int? id)
         {
@@ -74,6 +54,76 @@ namespace KVendasWeb.Controllers
             {
                 return View(obj);
             }
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerServices.FindById(id.Value);
+            if (obj == null)
+            {
+                return ViewBag.Id = id.Value;
+            }
+            else
+            {
+                List<Departament> departaments = _departamentService.FindAll();
+                SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departaments = departaments };
+                return View(viewModel);
+            }
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                try
+                {
+                    _sellerServices.Update(seller);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (NotFoundException)
+                {
+                    return NotFound();
+                }
+                catch (DbConcurrencyException)
+                {
+                    return BadRequest();
+                }
+            }
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerServices.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerServices.Remove(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- KVendasWeb/Services/SellerService.cs
+++ KVendasWeb/Services/SellerService.cs
@@ -2,21 +2,18 @@
 using KVendasWeb.Models;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore;
+using KVendasWeb.Services.Exceptions;
 
 namespace KVendasWeb.Services
 {
     public class SellerService
     {
         private readonly KVendasWebContext _context;
+
         public SellerService(KVendasWebContext context) 
         {
             _context = context;
-        }
-
-        public List<Seller> FindAll()
-        {
-            return _context.Seller.ToList(); 
         }
 
         public void Insert(Seller obj)
@@ -25,9 +22,31 @@ namespace KVendasWeb.Services
             _context.SaveChanges();
         }
 
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList(); 
+        }
+
         public Seller FindById(int id)
         {
             return _context.Seller.Include(obj => obj.Departament).FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new KeyNotFoundException("Vendedor não encontrado!");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
 
         public void Remove(int id)
