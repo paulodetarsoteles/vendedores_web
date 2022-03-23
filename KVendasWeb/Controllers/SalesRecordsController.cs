623cfcc3bf696b58d423d3121b3c67ce7954c362
paulodetarsoteles <potassio@hotmail.com>
2022-03-22 21:01:15 -0300

    implantacao da busca agrupada

--- KVendasWeb/Controllers/SalesRecordsController.cs
+++ KVendasWeb/Controllers/SalesRecordsController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using KVendasWeb.Models;
+using KVendasWeb.Services;
 using Microsoft.AspNetCore.Mvc;
-using KVendasWeb.Services; 
 
 namespace KVendasWeb.Controllers
 {
@@ -33,9 +36,20 @@ namespace KVendasWeb.Controllers
             var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
             return View(result);
         }
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
